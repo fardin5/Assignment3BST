0898250f9bc3718b5b1ec678bd162839b6dd2e0c
Tommy-La <tien.la@edu.sait.ca>
2023-11-29 15:52:24 -0700

    Added BSTree
    private class for iterator is un implemented

---
+++ src/utilities/BSTree.java
@@ -0,0 +1,178 @@
+package utilities;
+
+import java.util.NoSuchElementException;
+
+import exceptions.TreeException;
+import referenceBasedTreeImplementation.BSTreeNode;
+
+public class BSTree<E extends Comparable<? super E>> implements BSTreeADT<E> {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private BSTreeNode<E> root;
+
+    public BSTree() {
+        this.root = null;
+    }
+
+    @Override
+    public BSTreeNode<E> getRoot() throws TreeException {
+        if (isEmpty()) {
+            throw new TreeException("Tree is empty.");
+        }
+        return root;
+    }
+
+    @Override
+    public int getHeight() {
+        return calculateHeight(root);
+    }
+
+    private int calculateHeight(BSTreeNode<E> node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = calculateHeight(node.getLeftChild());
+        int rightHeight = calculateHeight(node.getRightChild());
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+
+    @Override
+    public int size() {
+        return countNodes(root);
+    }
+
+    private int countNodes(BSTreeNode<E> node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + countNodes(node.getLeftChild()) + countNodes(node.getRightChild());
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    @Override
+    public void clear() {
+        root = null;
+    }
+
+    @Override
+    public boolean contains(E entry) throws TreeException {
+        return search(entry) != null;
+    }
+
+    @Override
+    public BSTreeNode<E> search(E entry) throws TreeException {
+        return searchNode(root, entry);
+    }
+
+    private BSTreeNode<E> searchNode(BSTreeNode<E> node, E entry) {
+        if (node == null) {
+            return null;
+        }
+
+        int compareResult = entry.compareTo(node.getData());
+        if (compareResult == 0) {
+            return node;
+        } else if (compareResult < 0) {
+            return searchNode(node.getLeftChild(), entry);
+        } else {
+            return searchNode(node.getRightChild(), entry);
+        }
+    }
+
+    @Override
+    public boolean add(E newEntry) throws NullPointerException {
+        if (newEntry == null) {
+            throw new NullPointerException("Cannot add null entry to the tree.");
+        }
+
+        root = addNode(root, newEntry);
+        return true; // Assuming the add operation always succeeds in this implementation.
+    }
+
+    private BSTreeNode<E> addNode(BSTreeNode<E> node, E newEntry) {
+        if (node == null) {
+            return new BSTreeNode<>(newEntry);
+        }
+
+        int compareResult = newEntry.compareTo(node.getData());
+        if (compareResult < 0) {
+            node.setLeftChild(addNode(node.getLeftChild(), newEntry));
+        } else if (compareResult > 0) {
+            node.setRightChild(addNode(node.getRightChild(), newEntry));
+        }
+
+        return node;
+    }
+
+    @Override
+    public Iterator<E> inorderIterator() {
+        return new InorderIterator();
+    }
+
+    @Override
+    public Iterator<E> preorderIterator() {
+        return new PreorderIterator();
+    }
+
+    @Override
+    public Iterator<E> postorderIterator() {
+        return new PostorderIterator();
+    }
+
+    private class InorderIterator implements Iterator<E> {
+        // Implement the inorder iterator logic
+        // ...
+
+        @Override
+        public boolean hasNext() {
+            // Implement hasNext method for inorder iterator
+            return false;
+        }
+
+        @Override
+        public E next() throws NoSuchElementException {
+            // Implement next method for inorder iterator
+            return null;
+        }
+    }
+
+    private class PreorderIterator implements Iterator<E> {
+        // Implement the preorder iterator logic
+        // ...
+
+        @Override
+        public boolean hasNext() {
+            // Implement hasNext method for preorder iterator
+            return false;
+        }
+
+        @Override
+        public E next() throws NoSuchElementException {
+            // Implement next method for preorder iterator
+            return null;
+        }
+    }
+
+    private class PostorderIterator implements Iterator<E> {
+        // Implement the postorder iterator logic
+        // ...
+
+        @Override
+        public boolean hasNext() {
+            // Implement hasNext method for postorder iterator
+            return false;
+        }
+
+        @Override
+        public E next() throws NoSuchElementException {
+            // Implement next method for postorder iterator
+            return null;
+        }
+    }
+}
